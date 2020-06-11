9eb85bf03c4f4a59952477397bdce9b164566f85
harshitsoni01 <soniharshit2000@gmail.com>
2020-06-12 02:07:59 +0530

    updated readme

--- kinter.py
+++ kinter.py
@@ -1,5 +1,23 @@
 from tkinter import *
-m = Tk() 
-thelabel = Label(m, text = "This is easy!!")
-thelabel.pack()
+m = Tk()#initializing the tkinter window
+m.title("Title of GUI")
+m.geometry("600x600")#size of the window # its "x" not "*"
+thelabel = Label(m, text = "Welcome to GUI!")
+thelabel.pack(side = TOP)#pack method for placements of objects
+b1 = Button(m, text = "Button1", fg="white", bg="black")
+b1.pack(side=LEFT)# (side = top, bottom, )
+b2 = Button(m, text = "button2", font="comicsans, 14", width=15, height=2)
+b2.pack(side=RIGHT)
+#b3 = Button(m, text = "Button3")
+#b3.grid(row = 1, column = 0)# grid method for placement of objects 
+# you cannot mix grid and pack method
+#the values of row=1,col=0 depend of the position of the other widget in your window
+def close_window():
+    m.destroy()
+b4 = Button(m, text= "Exit",command = close_window)
+b4.place(relx=0.5, rely=0.5, anchor=CENTER)
+#this is udes to get image in tkinter
+#img = ImageTk.PhotoImage(Image.open("True1.gif"))
+#panel = Label(root, image = img)
+#panel.pack(side = "bottom", fill = "both", expand = "yes")
 m.mainloop() 
---
+++ readme.md
@@ -0,0 +1,29 @@
+# Python Basic Projects
+
+## Using different Libraies
+
+## **Api_weather:**
+
+### This program retrieves real time weather data from the internet using api function
+
+[Get api_key](https://openweathermap.org/api)
+
+## Dice
+
+### This program rolls the dice(s) (max two) as per the inserted number of dices
+
+[pip install random](https://pypi.org/project/random2/)
+
+## Guess
+
+### This is a 'guess a number' game with credit system and you can also increase or decrease the range of number
+
+## Madlibs
+
+### This is a madlibs game and you can use this to make different madlibs game as per your requirement
+
+[MORE MADLIBS](http://www.madlibs.com/printables/)
+
+## kinter
+
+### This program inserts buttons and images in tkinter
